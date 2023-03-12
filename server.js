37d7e3d547c289a86f1b1a69e4c586291605a638
JJH <skkuwjswnsgur456@gmail.com>
2023-03-12 19:59:27 +0900

    3rd

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+const users = ["TOM","ANDY","joonHyeok"];
+
+// let server = http.createServer(function (request,response){
+//     // response.end("<h1>Hello world</h1>");
+//     console.log(request.url);
+
+//     if(request.url ==="/"){
+//         response.end("<h1>Hello world</h1>")
+//     }else if(request.url === '/users'){
+//         response.end("<h1>"+users+'</h1>');
+//     }else{
+//         response.end("Page not Available");
+//     }
+// });
+
+app.get('/',(request,response)=>{
+    response.end("<h1>Hello world</h1>");
+})
+
+app.get('/users',(request,response)=>{
+    response.end(`<h1>${users}</h1>`);
+})
+
+app.get('/users/:id',(request,response)=>{
+    console.log(request.params);
+    const userName = users[request.params.id-1];
+    response.end(`<h1>${userName}</h1>`);
+})
+
+app.get('*',(request,response)=>{
+    response.end("<h1>Page not Available</h1>");
+})
+ 
+app.listen(3000);
+
+// server.listen(3000);
+
--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log('Hello');
+let url = new URL("https://eportal.skku.edu/wps/myportal/std/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zijQIsnD2c3Q38LcxNnQwC_YNMvPy9gwxCzY31w_Wj9KOASgxQgKOBU5CRk7GBgbu_EVYFyEr1g7Pz9Quys9McHRUVAdg81w4!/dz/d5/L2dBISEvZ0FBIS9nQSEh/")
+
+console.log(url.protocol);
+console.log(url.host);
+console.log(url.pathname);
+console.log(url.search);
